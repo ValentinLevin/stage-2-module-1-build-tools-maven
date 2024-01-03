a7bc17cd3eca1ef24a20518fefad5e5bd88710aa
Valentin Levin <levin_valentin@mail.ru>
2024-01-03 14:04:47 +0600

    solution #3.1

--- utils/src/test/java/com/epam/utils/StringUtilsTest.java
+++ utils/src/test/java/com/epam/utils/StringUtilsTest.java
@@ -1,6 +1,5 @@
-package com.epam;
+package com.epam.utils;
 
-import com.epam.utils.StringUtils;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
