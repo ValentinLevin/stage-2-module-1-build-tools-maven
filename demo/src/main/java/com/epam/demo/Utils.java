466bc0b2415a2b8555f09d0cf353805d792474df
Valentin Levin <levin_valentin@mail.ru>
2024-02-04 00:18:27 +0600

    Revert "solution #7"
    This reverts commit a41e246c48993ce26304ba6de21635f5358ad450.

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,15 +1,11 @@
 package com.epam.demo;
 
 import com.epam.utils.StringUtils;
+
 import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-        for (String arg : args) {
-            if (!StringUtils.isPositiveNumber(arg)) {
-                return false;
-            }
-        }
-        return true;
+        return args != null && !args.isEmpty() && args.stream().allMatch(StringUtils::isPositiveNumber);
     }
 }
